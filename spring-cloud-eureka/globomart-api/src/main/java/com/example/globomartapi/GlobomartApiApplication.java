491ffb281f0f76f14c70cd3b97d6d689eedde7c1
Ajay Kumar <ajay.ishan06@gmail.com>
2018-07-31 01:07:39 +0530

    Initial Setup

---
+++ spring-cloud-eureka/globomart-api/src/main/java/com/example/globomartapi/GlobomartApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.globomartapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobomartApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobomartApiApplication.class, args);
+	}
+}
